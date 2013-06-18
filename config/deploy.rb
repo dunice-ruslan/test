8f49d3f92c3ee47701930f8e9fadd22931aa2fb3
Ruslan Romanenko <romanenko.ruslan.dunice@gmail.com>
2013-06-18 18:29:58 +0400

    3

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,18 +48,8 @@ set :rvm_ruby_string, "1.9.3"
 set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake"
 set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
 
-
-# Настройка системы контроля версий и репозитария,
-# по умолчанию - git, если используется иная система версий,
-# нужно изменить значение scm.
 set :scm,             :git
-
-# Предполагается, что вы размещаете репозиторий Git в вашем
-# домашнем каталоге в подкаталоге git/<имя проекта>.git.
-# Подробнее о создании репозитория читайте в нашем блоге
-# http://locum.ru/blog/hosting/git-on-locum
 set :repository,      "git@github.com:dunice-ruslan/test.git"
-
 
 before 'deploy:finalize_update', 'set_current_release'
 task :set_current_release, :roles => :app do
@@ -67,25 +57,4 @@ task :set_current_release, :roles => :app do
 end
 
 
-set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
-
-
-
-# - for unicorn - #
-namespace :deploy do
-  desc "Start application"
-  task :start, :roles => :app do
-    run unicorn_start_cmd
-  end
-
-  desc "Stop application"
-  task :stop, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
-  end
-end
 
