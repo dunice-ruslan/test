6457da93616dfd2bd0881ab122d77941f5fea3c3
Ruslan Romanenko <romanenko.ruslan.dunice@gmail.com>
2013-06-18 18:23:50 +0400

    3

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@
 # закомментируйте эту строку.
 require 'bundler/capistrano'
 
-#load 'deploy/assets'
+load 'deploy/assets'
 
 # Для удобства работы мы рекомендуем вам настроить авторизацию
 # SSH по ключу. При работе capistrano будет использоваться
@@ -36,12 +36,13 @@ set :user,            "instamart"
 set :login,           "instamart"
 set :use_sudo,        false
 set :deploy_to,       "/home/#{user}/#{application}"
+set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
+set :unicorn_pid,     "/var/run/unicorn/#{application}.#{login}.pid"
 set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
 role :web,            deploy_server
 role :app,            deploy_server
 role :db,             deploy_server, :primary => true
 
-default_run_options[:shell] = true
 # Следующие строки необходимы, т.к. ваш проект использует rvm.
 set :rvm_ruby_string, "1.9.3"
 set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake"
@@ -59,13 +60,32 @@ set :scm,             :git
 # http://locum.ru/blog/hosting/git-on-locum
 set :repository,      "git@github.com:dunice-ruslan/test.git"
 
-## Если ваш репозиторий в GitHub, используйте такую конфигурацию
-# set :repository,    "git@github.com:username/project.git"
-
-## --- Ниже этого места ничего менять скорее всего не нужно ---
 
 before 'deploy:finalize_update', 'set_current_release'
 task :set_current_release, :roles => :app do
   set :current_release, latest_release
+end
+
+
+set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+
+
+
+# - for unicorn - #
+namespace :deploy do
+  desc "Start application"
+  task :start, :roles => :app do
+    run unicorn_start_cmd
+  end
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
+  end
 end
 
